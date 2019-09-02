e754983fff4a8959b0d29fc8e3c12fe6ba47fd61
Cherkashin Mikhail <cherkashinmikhailv@gmail.com>
2019-09-02 22:33:01 +0600

    Тайлы города, заполнение фона, запросы на сохранение позиции

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ var container = new PIXI.Container({
 container.y = 50;
 container.x = 50;
 
-var bg = new PIXI.Sprite(PIXI.Texture.WHITE);
-bg.tint = 0xff0000;
+const bgTexture =  PIXI.Texture.fromImage('./res/city_tiles/tile_0028.png');
+
+var bg = new PIXI.TilingSprite(bgTexture);
+
 bg.width = 10000;
 bg.height = 5000;
-
 container.addChild(bg);
 
 PIXI.loader
@@ -55,7 +56,7 @@ function setup() {
     right = keyboard('d');
 
   left.press = () => {
-    cat.vx = -5;
+    cat.vx = -1;
     cat.vy = 0;
   };
   
@@ -66,7 +67,7 @@ function setup() {
   };
 
   up.press = () => {
-    cat.vy = -5;
+    cat.vy = -1;
     cat.vx = 0;
   };
   up.release = () => {
@@ -76,7 +77,7 @@ function setup() {
   };
 
   right.press = () => {
-    cat.vx = 5;
+    cat.vx = 1;
     cat.vy = 0;
   };
   right.release = () => {
@@ -86,7 +87,7 @@ function setup() {
   };
 
   down.press = () => {
-    cat.vy = 5;
+    cat.vy = 1;
     cat.vx = 0;
   };
   down.release = () => {
@@ -112,13 +113,31 @@ function setup() {
     ) {
       console.log('BORDER')
     } else {
-
+      
       container.pivot.x += cat.vx;
       container.pivot.y += cat.vy;
+
+      let newCatX = cat.x + cat.vx;
+      let newCatY = cat.y + cat.vy;
+
+      if (newCatX !== cat.x || newCatY !== cat.y) {
+        axios.post('http://127.0.0.1:8080/api/v1/user/set_position',{
+          xAxis: cat.x,
+          yAxis: cat.y
+        },{
+          headers: {
+            'content-type': 'application/json',
+            'token': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBdXRoIHRva2VuIiwicm9sZXMiOiJVc2VyIiwiaXNzIjoiZHVjYXQtc3ByaW5nYm9vdC1qd3R0b2tlbiIsInVzZXJJZCI6MSwiaWF0IjoiMjAxOS0wOS0wMiAyMToyMjozMSJ9.Pqp7BI3Go0NA9PewaHWq73L4m7C8kpRiwA9ERQxMe6340FO1gRai2OVBeUHnX4kQS4Kq8RPs2dso8ltfgfRY4w'
+          }
+        }).then((response) => {
+          console.log(response)
+        })
+      }
       cat.x += cat.vx;
       cat.y += cat.vy;
+
+      
     }
     
   }
 }
-
